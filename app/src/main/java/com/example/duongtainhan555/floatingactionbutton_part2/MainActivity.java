445fb0ef57164a7c944415848adcbccf43b7b37f
Dương Tài Nhân <15521044@gm.uit.edu.vn>
2018-08-04 15:21:10 +0700

    Way back use animation

--- app/src/main/java/com/example/duongtainhan555/floatingactionbutton_part2/MainActivity.java
+++ app/src/main/java/com/example/duongtainhan555/floatingactionbutton_part2/MainActivity.java
@@ -4,16 +4,17 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.FrameLayout;
-import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
     private FloatingActionButton fab_app, fab_facebook, fab_chrome,fab_twitch, fab_skype;
     private Animation move_left, move_right, move_top, move_bottom;
+    private boolean status = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,10 +27,23 @@ public class MainActivity extends AppCompatActivity {
         fab_app.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                MoveLeft();
-                MoveRight();
-                MoveTop();
-                MoveBottom();
+                if(status)
+                {
+                    MoveLeft(move_left);
+                    MoveRight(move_right);
+                    MoveTop(move_top);
+                    MoveBottom(move_bottom);
+                    status = false;
+                }
+                else
+                {
+                    MoveRight(move_left);
+                    MoveLeft(move_right);
+                    MoveTop(move_bottom);
+                    MoveBottom(move_top);
+                    status = true;
+                }
+
             }
         });
         fab_facebook.setOnClickListener(new View.OnClickListener() {
@@ -74,35 +88,74 @@ public class MainActivity extends AppCompatActivity {
         move_top = AnimationUtils.loadAnimation(this,R.anim.move_top);
         move_bottom = AnimationUtils.loadAnimation(this,R.anim.move_bottom);
     }
-    private void MoveLeft()
+    private void MoveLeft(Animation animation)
     {
-        FrameLayout.LayoutParams params_left = (FrameLayout.LayoutParams) fab_facebook.getLayoutParams();
-        params_left.rightMargin = (int)(fab_facebook.getWidth()*1.7);
-        fab_facebook.setLayoutParams(params_left);
-        fab_facebook.startAnimation(move_left);
+        if(status)
+        {
+            FrameLayout.LayoutParams params_left = (FrameLayout.LayoutParams) fab_facebook.getLayoutParams();
+            params_left.rightMargin = (int)(fab_facebook.getWidth()*1.7);
+            fab_facebook.setLayoutParams(params_left);
+            fab_facebook.startAnimation(animation);
+        }
+        else
+        {
+            FrameLayout.LayoutParams params_left = (FrameLayout.LayoutParams) fab_facebook.getLayoutParams();
+            params_left.rightMargin -= (int)(fab_facebook.getWidth()*1.7);
+            fab_facebook.setLayoutParams(params_left);
+            fab_facebook.startAnimation(animation);
+        }
     }
-    private void MoveRight()
+    private void MoveRight(Animation animation)
     {
-        FrameLayout.LayoutParams params_right = (FrameLayout.LayoutParams) fab_chrome.getLayoutParams();
-        params_right.leftMargin = (int)(fab_chrome.getWidth()*1.7);
-        fab_chrome.setLayoutParams(params_right);
-        fab_chrome.startAnimation(move_right);
+        if(status)
+        {
+            FrameLayout.LayoutParams params_right = (FrameLayout.LayoutParams) fab_chrome.getLayoutParams();
+            params_right.leftMargin = (int)(fab_chrome.getWidth()*1.7);
+            fab_chrome.setLayoutParams(params_right);
+            fab_chrome.startAnimation(animation);
+        }
+        else
+        {
+            FrameLayout.LayoutParams params_right = (FrameLayout.LayoutParams) fab_chrome.getLayoutParams();
+            params_right.leftMargin -= (int)(fab_chrome.getWidth()*1.7);
+            fab_chrome.setLayoutParams(params_right);
+            fab_chrome.startAnimation(animation);
+        }
+    }
+    private void MoveTop(Animation animation)
+    {
+        if(status)
+        {
+            FrameLayout.LayoutParams params_top = (FrameLayout.LayoutParams) fab_twitch.getLayoutParams();
+            params_top.bottomMargin = (int)(fab_twitch.getWidth()*1.7);
+            fab_twitch.setLayoutParams(params_top);
+            fab_twitch.startAnimation(animation);
+        }
+        else
+        {
+            FrameLayout.LayoutParams params_top = (FrameLayout.LayoutParams) fab_twitch.getLayoutParams();
+            params_top.bottomMargin -= (int)(fab_twitch.getWidth()*1.7);
+            fab_twitch.setLayoutParams(params_top);
+            fab_twitch.startAnimation(animation);
+        }
 
     }
-    private void MoveTop()
+    private void MoveBottom(Animation animation)
     {
-        FrameLayout.LayoutParams params_top = (FrameLayout.LayoutParams) fab_twitch.getLayoutParams();
-        params_top.bottomMargin = (int)(fab_twitch.getHeight()*1.7);
-        fab_twitch.setLayoutParams(params_top);
-        fab_twitch.startAnimation(move_top);
-    }
-    private void MoveBottom()
-    {
-        FrameLayout.LayoutParams params_bottom = (FrameLayout.LayoutParams) fab_skype.getLayoutParams();
-        params_bottom.topMargin = (int)(fab_skype.getHeight()*1.7);
-        fab_skype.setLayoutParams(params_bottom);
-        fab_skype.startAnimation(move_bottom);
-
+        if(status)
+        {
+            FrameLayout.LayoutParams params_bottom = (FrameLayout.LayoutParams) fab_skype.getLayoutParams();
+            params_bottom.topMargin = (int)(fab_skype.getWidth()*1.7);
+            fab_skype.setLayoutParams(params_bottom);
+            fab_skype.startAnimation(animation);
+        }
+        else
+        {
+            FrameLayout.LayoutParams params_bottom = (FrameLayout.LayoutParams) fab_skype.getLayoutParams();
+            params_bottom.topMargin -= (int)(fab_skype.getWidth()*1.7);
+            fab_skype.setLayoutParams(params_bottom);
+            fab_skype.startAnimation(animation);
+        }
     }
 
 }
